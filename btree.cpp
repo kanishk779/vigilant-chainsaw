ee50bfc89edc27202550668b2c197ede9f6fed88
kanishk779 <kashyapamankumar54@gmail.com>
2021-02-19 14:22:26 +0530

    Remove debug statements and Add test generator

--- btree.cpp
+++ btree.cpp
@@ -80,7 +80,7 @@ int give_size(Node * node){
 }
 void recalculate_subtree_sizes(Node * node){
 	// update the subtree sizes for all ancestors
-	cout<<"recalculate_subtree_sizes\n";
+	// cout<<"recalculate_subtree_sizes\n";
 	while(node->parent != NULL){
 		rep(i,0,3){
 			assert(node != NULL);
@@ -90,29 +90,29 @@ void recalculate_subtree_sizes(Node * node){
 	}
 }
 void insert_internal(Node * node, ToBeInserted * ins){
-	cout<<"trying to insert "<<ins->val<<"\n";
+	// cout<<"trying to insert "<<ins->val<<"\n";
 	if(node == NULL){
 		// create a new root node
-		cout<<"creating new root\n";
+		// cout<<"creating new root\n";
 		root = give_node();
 		root->keys[0] = ins->val;
 		root->children[0] = ins->left;
 		root->children[1] = ins->right;
-		cout<<"left val "<<ins->left->keys[0]<<"\n";
-		cout<<"right val "<<ins->right->keys[0]<<"\n";
-		cout<<"right size "<<ins->right->curr_size<<"\n";
+		// cout<<"left val "<<ins->left->keys[0]<<"\n";
+		// cout<<"right val "<<ins->right->keys[0]<<"\n";
+		// cout<<"right size "<<ins->right->curr_size<<"\n";
 		root->children[0]->parent = root;
 		root->children[1]->parent = root;
 		root->subtree_size[0] = give_size(ins->left);
 		root->subtree_size[1] = give_size(ins->right);
 		return;
 	}
-	cout<<"curr_size for node is "<<node->curr_size<<"\n";
-	cout<<"first key of node is "<<node->keys[0]<<"\n";
+	// cout<<"curr_size for node is "<<node->curr_size<<"\n";
+	// cout<<"first key of node is "<<node->keys[0]<<"\n";
 	if(node->curr_size == 1){
-		cout<<"current node size is one(insert_internal)\n";
+		// cout<<"current node size is one(insert_internal)\n";
 		if(ins->val < node->keys[0]){
-			cout<<"insert at index 0\n";
+			// cout<<"insert at index 0\n";
 			node->children[2] = node->children[1];
 			node->subtree_size[2] = node->subtree_size[1];
 			node->children[1] = ins->right;
@@ -123,7 +123,7 @@ void insert_internal(Node * node, ToBeInserted * ins){
 			node->keys[0] = ins->val;
 		}
 		else{
-			cout<<"insert at index 1\n";
+			// cout<<"insert at index 1\n";
 			node->children[1] = ins->left;
 			node->subtree_size[1] = give_size(ins->left);
 			node->children[2] = ins->right;
@@ -136,7 +136,7 @@ void insert_internal(Node * node, ToBeInserted * ins){
 		// find the position where this node to be inserted will actually be inserted
 		if(ins->val < node->keys[0]){
 			// insert at the start at index -> 0
-			cout<<"inserting at index 0\n";
+			// cout<<"inserting at index 0\n";
 			for(int i=3;i>=2;i--){
 				node->children[i] = node->children[i-1];
 				node->subtree_size[i] = node->subtree_size[i-1];
@@ -150,7 +150,7 @@ void insert_internal(Node * node, ToBeInserted * ins){
 		}
 		else if(ins->val > node->keys[0] && ins->val < node->keys[1]){
 			// insert at index -> 1
-			cout<<"inserting at index 1\n";
+			// cout<<"inserting at index 1\n";
 			node->children[3] = node->children[2];
 			node->subtree_size[3] = node->subtree_size[2];
 			node->children[2] = ins->right;
@@ -163,7 +163,7 @@ void insert_internal(Node * node, ToBeInserted * ins){
 		}
 		else{
 			// insert at index -> 2
-			cout<<"inserting at index 2\n";
+			// cout<<"inserting at index 2\n";
 			node->children[3] = ins->right;
 			node->subtree_size[3] = give_size(ins->right);
 			node->children[2] = ins->left;
@@ -178,7 +178,7 @@ void insert_internal(Node * node, ToBeInserted * ins){
 		leftc->keys[0] = node->keys[0];
 
 		if(node->is_leaf){
-			cout<<"The node split is leaf node\n";
+			// cout<<"The node split is leaf node\n";
 			leftc->subtree_size[0] = node->subtree_size[0];
 			leftc->subtree_size[1] = 0;
 			
@@ -231,14 +231,14 @@ void insert_internal(Node * node, ToBeInserted * ins){
 }
 void insert(Node * node, int val){
 	if(root == NULL){
-		cout<<"root is NULL\n";
+		// cout<<"root is NULL\n";
 		root = give_node();
 		root->keys[0] = val;
 		root->subtree_size[0] = 1;
 		root->is_leaf = true;
 		return;
 	}
-	cout<<"going to leaf\n";
+	// cout<<"going to leaf\n";
 	assert(node != NULL);
 	while(node->is_leaf == false){
 		if(val < node->keys[0])
@@ -255,9 +255,9 @@ void insert(Node * node, int val){
 		}
 		assert(node != NULL);
 	}
-	cout<<"reach leaf\n";
+	// cout<<"reach leaf\n";
 	if(node->curr_size == 1){
-		cout<<"leaf size is one\n";
+		// cout<<"leaf size is one\n";
 		if(val == node->keys[0]){
 			node->subtree_size[0]++;
 		}
@@ -277,22 +277,22 @@ void insert(Node * node, int val){
 		split_left = node;
 	}
 	else{
-		cout<<"leaf size is two\n";
+		// cout<<"leaf size is two\n";
 		if(val == node->keys[0] || val == node->keys[1]){
-			cout<<"duplicate entry\n";
+			// cout<<"duplicate entry\n";
 			if(val == node->keys[0]){
 				node->subtree_size[0]++;
-				cout<<"new size "<<node->subtree_size[0]<<"\n";
+				// cout<<"new size "<<node->subtree_size[0]<<"\n";
 			}
 			else if(val == node->keys[1]){
 				node->subtree_size[1]++;
-				cout<<"new size "<<node->subtree_size[1]<<"\n";
+				// cout<<"new size "<<node->subtree_size[1]<<"\n";
 			}
 			split_left = node;
 		}
 		else{
 			// split and call
-			cout<<"splitting and call\n";
+			// cout<<"splitting and call\n";
 			ToBeInserted * ins = new ToBeInserted;
 			ins->val = val;
 			ins->left = ins->right = NULL;
@@ -374,19 +374,19 @@ int find_leq(Node * node, int val){
 void dfs(Node * node, int d){
 	string s((size_t) 4*d, ' ');
 	if(node != NULL){
-		cout<<s;
-		cout<<"sub[0] : "<<node->subtree_size[0]<<"\n";
+		// cout<<s;
+		// cout<<"sub[0] : "<<node->subtree_size[0]<<"\n";
 		cout<<s;
 		cout<<node->keys[0]<<"\n";
-		cout<<s;
-		cout<<"sub[1] : "<<node->subtree_size[1]<<"\n";
+		// cout<<s;
+		// cout<<"sub[1] : "<<node->subtree_size[1]<<"\n";
 		dfs(node->children[0], d+1);
 		dfs(node->children[1], d+1);
 		if(node->curr_size == 2){
 			cout<<s;
 			cout<<node->keys[1]<<"\n";
-			cout<<s;
-			cout<<"sub[2] : "<<node->subtree_size[2]<<"\n";
+			// cout<<s;
+			// cout<<"sub[2] : "<<node->subtree_size[2]<<"\n";
 			dfs(node->children[2], d+1);
 		}
 	}
@@ -426,7 +426,6 @@ int main(){
 	string s;
 	getline(cin, s);
 	while(!s.empty()){
-		cout<<s<<"\n";
 		istringstream iss(s);
 		vector<string> tokens{istream_iterator<string>{iss},istream_iterator<string>{}};
 		if(tokens[0][0] == 'I'){
@@ -439,26 +438,26 @@ int main(){
 				recalculate_subtree_sizes(split_left);
 			if(split_right != NULL)
 				recalculate_subtree_sizes(split_right);
-			dfs(root, 0);
+			// dfs(root, 0);
 		}
 		else if(tokens[0][0] == 'F'){
 			int x = stoi(tokens[1]);
 			II res = find_val(root, x);
 			if(res.F){
-				cout<<"found\n";
+				cout<<"f\n";
 			}
 			else
-				cout<<"not found\n";
+				cout<<"nf\n";
 		}
 		else if(tokens[0][0] == 'C'){
 			int x = stoi(tokens[1]);
-			cout<<"count : "<<count_val(x)<<"\n";
+			cout<<count_val(x)<<"\n";
 		}
 		else if(tokens[0][0] == 'R'){
 			int x = stoi(tokens[1]);
 			int y = stoi(tokens[2]);
 			int ans = range(root, x, y, min_key, max_key);
-			cout<<"range : "<<ans<<"\n";
+			cout<<ans<<"\n";
 		}
 		else{
 			cout<<"Sorry We don't do that Here\n";
---
+++ testGenerator.py
@@ -0,0 +1,20 @@
+import random
+lim = 1000000
+ops = 10
+f = open('input.txt', 'w')
+for i in range(ops):
+    j = random.randint(1, lim)
+    f.write('INSERT '+str(j) + '\n')
+
+for i in range(ops):
+    j = random.randint(1, lim)
+    f.write('FIND '+str(j) + '\n')
+
+for i in range(ops):
+    j = random.randint(1, lim)
+    f.write('COUNT '+str(j) + '\n')
+
+for i in range(ops):
+    r = random.randint(1, lim)
+    l = random.randint(1, r)
+    f.write('RANGE '+str(l)+' '+str(r)+'\n')
